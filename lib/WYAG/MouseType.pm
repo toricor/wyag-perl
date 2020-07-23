05f2e10382217a5f48b8594a6c806dbdd68169c9
toricor <toriyabe@mfac.jp>
2020-07-23 15:15:53 +0900

    fix GitObject type message

--- lib/WYAG/MouseType.pm
+++ lib/WYAG/MouseType.pm
@@ -28,7 +28,7 @@ use MouseX::Types::Mouse qw/Int Str HashRef Object ArrayRef Maybe Undef/;
 subtype GitObject,
     as Object(),
     where { ref($_) =~ /\AWYAG::GitObject::(Blob|Tag|Tree|Commit)\z/mo },
-    message { "GitObject is WYAG::GitObject::*. got: ". ref $_ };
+    message { "GitObject is WYAG::GitObject::(Blob|Tag|Tree|Commit). got: ". ref $_ };
 
 no Mouse::Util::TypeConstraints;
 1;
