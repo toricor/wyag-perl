c8f0ea7bbf4172afd898f1d055e9b5809ddec650
toricor <toriyabe@mfac.jp>
2020-07-26 00:29:55 +0900

    make GitRepository.pm again

---
+++ lib/WYAG/GitRepository.pm
@@ -0,0 +1,27 @@
+package WYAG::GitRepository;
+
+use strict;
+use warnings;
+
+use Mouse;
+use File::Spec;
+
+has worktree => (
+    is       => 'ro',
+    isa      => 'Str', # path
+    required => 1,
+);
+
+has gitdir => (
+    is      => 'ro',
+    isa     => 'Str',
+    builder => sub {
+        my $self = shift;
+        return File::Spec->catfile($self->worktree, '.git');
+    },
+);
+
+no Mouse;
+__PACKAGE__->meta->make_immutable;
+
+1;
